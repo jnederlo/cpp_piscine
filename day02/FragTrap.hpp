42dfafe29ea5dc69f7ece5e2b9e6e5f8e564f930
Jarvis Nederlof <jarvisnederlof@gmail.com>
2018-01-12 17:53:03 -0800

    day02

---
+++ day02/FragTrap.hpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jnederlo <jnederlo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/12 11:50:14 by jnederlo          #+#    #+#             */
+/*   Updated: 2018/01/12 17:26:00 by jnederlo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+#include <iostream>
+
+class FragTrap{
+
+public:
+    std::string name;
+    unsigned int hitPoints;
+    unsigned int maxHitPoints;
+    unsigned int energyPoints;
+    unsigned int maxEnergyPoints;
+    int level;
+    unsigned int meleeAttackDamage;
+    unsigned int rangedAttackDamage;
+    unsigned int armorDamageReduction;
+    unsigned int powerAttackDamage;
+
+    FragTrap( std::string name );                   //constructor
+    ~FragTrap( void );                              //destructor
+    FragTrap( FragTrap const & src );               //copy method
+    FragTrap & operator=( FragTrap const & rhs );   //assignment operator overload
+
+    unsigned int rangedAttack(std::string const & target);
+    unsigned int meleeAttack(std::string const & target);
+
+
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+    unsigned int vaulthunter_dot_exe(std::string const & target);
+    void setName(std::string name);
+
+
+};
+
+
+
+#endif
